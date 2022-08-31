45202fa75682408c4121450d6d29e14b2301133b
Celina <57647158+celina-lopez@users.noreply.github.com>
2022-08-31 09:41:47 -0700

    list images

--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -17,3 +17,12 @@ export const imageKey = (uid) => {
 export const metadataKey = (uid) => {
   return createKeyPath("metadata", `${uid}.json`);
 }
+
+export async function getImage(uid){
+  const data =  s3.getObject({
+    Bucket: process.env.AWS_BUCKET_NAME,
+    Key: imageKey(uid),
+  }).promise();
+
+  return "data:image/jpeg;base64," + encode(data.Body)
+}
