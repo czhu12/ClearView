6476d390281710a4fdfcfc1405835fb0189d8783
Celina <57647158+celina-lopez@users.noreply.github.com>
2022-09-02 08:49:27 -0700

    orc checker

--- frontend/src/utils/orc_checker.js
+++ frontend/src/utils/orc_checker.js
@@ -1,11 +1,27 @@
+import { createWorker } from 'tesseract.js';
 
-class ORCChecker {
-  constructor({image, location}) {
-    this.image = image;
-    this.location = location;
+export default class ORCChecker {
+  constructor(base64, { words }) {
+    this.base64 = base64;
+    this.words = words;
   }
 
-  presence(word) {
-    return true
+  wordInString(text, word) {
+    return new RegExp('\\b' + word + '\\b', 'i').test(text);
+  }
+
+  hasWords(text) {
+    return this.words.some(word => this.wordInString(text, word))
+  }
+
+  async start() {
+    const buffer = Buffer.from(this.base64.split(',')[1], "base64")
+    const worker = createWorker();
+    await worker.load();
+    await worker.loadLanguage('eng');
+    await worker.initialize('eng');
+    const { data: { text } } = await worker.recognize(buffer);
+    await worker.terminate();
+    return this.hasWords(text);
   }
 }
