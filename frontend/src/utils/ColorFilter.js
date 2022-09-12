360eca903618a9c195ac1909e96e1615620f37d0
Celina Lopez <marisa.celina.lopez4@gmail.com>
2022-09-11 21:57:17 -0700

    color count

--- frontend/src/utils/ColorFilter.js
+++ frontend/src/utils/ColorFilter.js
@@ -24,6 +24,45 @@ class Color {
   }
 }
 
+function getGrouping(imageData, coloredNodes) {
+  let groups = 0;
+  const checkedColoredNodes = {}
+
+  coloredNodes.map(x => checkedColoredNodes[x] = {checked: false, matches: 0});
+
+  function isSurrounded(index, currentMatches) {
+    let matches = currentMatches;
+    let neighbors = []                             // clear array
+    neighbors[0] = index - imageData.width * 4 - 4 // Upper left
+    neighbors[1] = index - imageData.width * 4     // Upper middle
+    neighbors[2] = index - imageData.width * 4 + 4 // Upper right
+    neighbors[3] = index - 4                       // left
+    neighbors[4] = index + 4                       // right
+    neighbors[5] = index + imageData.width * 4 - 4 // Lower left
+    neighbors[6] = index + imageData.width * 4     // lower middle
+    neighbors[7] = index + imageData.width * 4 + 4 // Lower right
+
+    checkedColoredNodes[index].checked = true
+    neighbors.map(neighbor => {
+      if (coloredNodes.includes(neighbor) && !checkedColoredNodes[neighbor].checked) {
+        matches++;
+        matches = isSurrounded(neighbor, matches)
+      }
+    })
+    return matches;
+  }
+
+  coloredNodes.map((coloredNode) => {
+    if (!checkedColoredNodes[coloredNode].checked) {
+      const matched = isSurrounded(coloredNode, 0);
+      if (matched > 4) groups++                    // More than 4 pixel grouping
+    }
+  });
+
+  return groups
+}
+
+
 export default class ColorFilter {
   constructor({ threshold, colorTarget, inputCanvasName, outputName }) {
     this.colorTarget = colorTarget;
@@ -37,11 +76,12 @@ export default class ColorFilter {
     const context = canvas.getContext("2d");
     const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
     const data = imageData.data;
+    let coloredNodes = []
 
     const outputCanvas = createCanvas(canvas.width, canvas.height);
     const outputContext = outputCanvas.getContext("2d");
 
-    const red = new Color(
+    const colorTarget = new Color(
       this.colorTarget[0],
       this.colorTarget[1],
       this.colorTarget[2],
@@ -49,11 +89,12 @@ export default class ColorFilter {
     for (let i = 0; i < data.length; i += 4) {
       const color = new Color(data[i], data[i + 1], data[i + 2]);
       const normalized = color.normalize();
-      const score = normalized.dot(red);
+      const score = normalized.dot(colorTarget);
       if (score > this.threshold) {
         data[i] = color.r;     // red
         data[i + 1] = color.g; // green
         data[i + 2] = color.b; // blue
+        coloredNodes.push(i)
       } else {
         data[i] = 0;     // red
         data[i + 1] = 0; // green
@@ -62,9 +103,11 @@ export default class ColorFilter {
     }
     outputContext.putImageData(imageData, 0, 0);
     state[this.outputName] = outputCanvas;
+    const numberOfColorGroups = getGrouping(imageData, coloredNodes)
     return {
-      result: true,
-      reason: null,
+      result: numberOfColorGroups,
+      reason: `Number of color groups: ${numberOfColorGroups}`,
     }
   }
+
 }
