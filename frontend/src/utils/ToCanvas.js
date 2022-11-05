b1a85a42476018ff1b9c3baf34cd4fb3df6c94c5
Chris Zhu <chriszhu@Chriss-MacBook-Pro.local>
2022-11-05 16:53:18 -0700

    fix bug with cropping

--- frontend/src/utils/ToCanvas.js
+++ frontend/src/utils/ToCanvas.js
@@ -9,12 +9,12 @@ export default class ToCanvas {
 
   async execute(state) {
     const image = await loadImage(state.base64);
-    const canvas = createCanvas(image.width, image.height);
+    const canvas = createCanvas(Math.max(image.width, image.height), Math.max(image.width, image.height));
     const ctx = canvas.getContext("2d");
     if (this.rotate) {
       ctx.translate(canvas.width/2,canvas.height/2);
       ctx.rotate(270 * Math.PI / 180)
-      ctx.drawImage(image,-this.width/2,-this.width/2);
+      ctx.drawImage(image,-this.width/2,-this.height/2);
     } else {
       ctx.drawImage(image, 0, 0)
     }
